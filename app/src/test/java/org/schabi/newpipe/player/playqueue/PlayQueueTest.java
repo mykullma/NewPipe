23744fb2bbdf328faafe111a88270b60de7a4926
mykullma <752830132@qq.com>
2023-02-27 16:14:27 -0800

    CI fail test

--- app/src/test/java/org/schabi/newpipe/player/playqueue/PlayQueueTest.java
+++ app/src/test/java/org/schabi/newpipe/player/playqueue/PlayQueueTest.java
@@ -223,6 +223,7 @@ public class PlayQueueTest {
         public void removeBeforeCurrent() {
             queue2.remove(2);
             assertEquals(2, queue2.getIndex());
+            assertTrue(false);
         }
     }
 }
